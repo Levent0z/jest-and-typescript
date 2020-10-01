c7d8fe42dbf510f69fa8df7d516f052ce4779c3f
Levent Oz <leventoz.dev@gmail.com>
2020-10-01 14:43:34 -0700

    Add Code Coverage

--- scripts/jest.config.js
+++ scripts/jest.config.js
@@ -8,5 +8,27 @@ module.exports = {
         'ts-jest': {
             tsConfig: '<rootDir>/tsconfig.test.json'
         }
-    }
+    },
+    coverageThreshold: {
+        // https://jestjs.io/docs/en/configuration#coveragethreshold-object
+        "global": {
+            "branches": 120,
+            "functions": 85,
+            "lines": 85,
+            "statements": 85
+        }
+    },
+    collectCoverage: true,
+    coverageReporters: ["json", "html"],
+    reporters: [
+        "default",
+        [
+            "./node_modules/jest-html-reporter",
+            {
+                "pageTitle": "Jest Test report",
+                "outputPath": "./jest-report/index.html",
+                "includeFailureMsg": true
+            }
+        ]
+    ]
 };

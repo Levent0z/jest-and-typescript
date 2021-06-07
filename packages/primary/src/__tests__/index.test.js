42a937fe04c02c73064e60eb890fe31f0d08c405
Levent Oz <leventoz.dev@gmail.com>
2021-06-07 11:44:07 -0700

    Add left-pad as example import

--- packages/primary/src/__tests__/index.test.js
+++ packages/primary/src/__tests__/index.test.js
@@ -6,6 +6,6 @@ describe('index', () => {
 
         expect(() => { main = new Main(); }).not.toThrow();
         expect(main.hello('world')).toBe('Hello world!');
-        expect(main.hello('jest')).toBe('Hello jest!');
+        expect(main.hello('jest')).toBe(' Hello jest!'); // Left padded
     });
 });
--- packages/primary/src/index.ts
+++ packages/primary/src/index.ts
@@ -1,5 +1,8 @@
+import leftPad from 'left-pad';
+
 export class Main {
     hello(name: string): string {
-        return `Hello ${name}!`;
+        const retVal: string = leftPad(`Hello ${name}!`, 12);
+        return retVal;
     }
 }
